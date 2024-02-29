25370fdd809cd4a4e27ffd1ffccca17ac95acccd
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-02-29 11:47:45 +0100

    Implements basic list styling

--- hello-react-world/hello-app/src/components/books/BooksList.tsx
+++ hello-react-world/hello-app/src/components/books/BooksList.tsx
@@ -1,7 +1,28 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
 export function BooksList({ books }: { books: Array<{ author: string, id: string, rating: number, title: string }> }) {
     return (
-        <ul>
-            {books.map((book) => <li key={book.id}>{book.title}</li>)}
-        </ul>
+        <Container style={{marginTop: 20, marginBottom: 20,}}>
+            <Row xs={1} md={2} className="g-4">
+                {books.map((book) => {
+                    const padding = { padding: 20 }
+                    const content = <Col key={book.id}>
+                        <Card border="primary" style={padding}>
+                            <Card.Img variant="top" src='/book-placeholder.svg' style={padding} />
+                            <Card.Body>
+                                <Card.Title>{book.title}</Card.Title>
+                                <Card.Text>{book.author}</Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    return content
+                })}
+            </Row>
+        </Container>
     );
 }
