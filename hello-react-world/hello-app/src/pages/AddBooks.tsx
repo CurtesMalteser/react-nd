fd6867d94e9f58f8c4155505b779da4b1e73d508
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-03-04 15:28:44 +0100

    Implements AddBooks
    * API call missing

--- hello-react-world/hello-app/src/pages/AddBooks.tsx
+++ hello-react-world/hello-app/src/pages/AddBooks.tsx
@@ -1,34 +1,63 @@
-import React, { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormSelect from 'react-bootstrap/FormSelect'
 
 
 function AddBooks() {
-    const [bookTitle, setBookTitle] = useState('');
-    const [author, setAuthor] = useState('');
 
-    const handleTitleChange = (e: { target: { value: React.SetStateAction<string> } }) => {
-        setBookTitle(e.target.value);
+    const [rating, setRating] = useState(0);
+    const [validated, setValidated] = useState(false);
+
+    const handleSelectChange = (event: FormEvent<HTMLSelectElement>) => {
+        setRating(event.currentTarget.value as unknown as number);
     };
 
-    const handleAuthorChange = (e: { target: { value: React.SetStateAction<string> } }) => {
-        setAuthor(e.target.value);
-    };
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
 
-    const handleAddBook = () => {
-        // Add your logic here to post book to the server
-        console.log('Book added:', bookTitle, 'by', author);
-
-        // Implement form validation here
-        // Reset the input fields if request is successful
-        setBookTitle('');
-        setAuthor('');
+        setValidated(true);
     };
 
     return (
         <Container style={{ marginTop: '20px', marginBottom: '20', }}>
-            <input type="text" value={bookTitle} onChange={handleTitleChange} placeholder="Enter book title" />
-            <input type="text" value={author} onChange={handleAuthorChange} placeholder="Enter author" />
-            <button onClick={handleAddBook}>Add Book</button>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formTitle">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control required type="text" placeholder="Enter title" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formAuthor">
+                        <Form.Label>Author</Form.Label>
+                        <Form.Control required type="text" placeholder="Enter author" />
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formISBN">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control required type="text" placeholder="Enter ISBN" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formRating">
+                        <Form.Label>Rating</Form.Label>
+                        <FormSelect value={rating} aria-label="Default select example" isValid={rating > 0 && rating <= 5} isInvalid={rating == 0 || rating >5} onChange={handleSelectChange}>
+                            <option value="0">---</option>
+                            <option value="1">One out of Five</option>
+                            <option value="2">Two out of Five</option>
+                            <option value="3">Three out of Five</option>
+                            <option value="4">Four out of Five</option>
+                            <option value="5">Five out of Five</option>
+                        </FormSelect>
+                    </Form.Group>
+                </Row>
+                <Button type='submit'>Add Book</Button>
+            </Form>
         </Container>
     );
 }
