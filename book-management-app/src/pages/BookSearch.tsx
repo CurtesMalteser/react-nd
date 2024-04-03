a2ea098e7b5a6c1b9fcdc4c15c8db6b8788381c7
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-04-03 11:09:31 +0200

    Wraps BookSearch with Container

--- book-management-app/src/pages/BookSearch.tsx
+++ book-management-app/src/pages/BookSearch.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { search } from '../utils/BooksAPI';
 import BookShelf from '../components/books/BookShelf';
 import Book from '../components/books/Book';
+import Container from 'react-bootstrap/Container';
 
 
 function BookSearch() {
@@ -28,7 +29,7 @@ function BookSearch() {
     };
 
     return (
-        <div>
+        <Container style={{ marginTop: 20, marginBottom: 20, }}>
             <input
                 type="text"
                 value={searchTerm}
@@ -36,7 +37,7 @@ function BookSearch() {
             />
             <button onClick={() => searchHandler(searchTerm)}>Search</button>
            {(searchResults.length > 0) && <BookShelf title={`Search Results: ${searchResults.length}`} books={searchResults} />}
-        </div>
+        </Container>
     );
 };
 
