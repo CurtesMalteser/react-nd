fd678ac6a61c72c9a6a4937fb6c0dafffe050531
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-06-03 16:13:44 +0200

    Implements title, subtitle and button to submit new poll

--- employee-polls-app/src/pages/NewPollPage.tsx
+++ employee-polls-app/src/pages/NewPollPage.tsx
@@ -1,7 +1,35 @@
-function NewQuestionPage() {
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { useAppSelector } from "../app/hooks";
+import Button from 'react-bootstrap/Button';
+import { userID as userIDSelector } from "../features/authedUser/authedUserSlice";
+
+function NewPollPage() {
+
+    const userID = useAppSelector(userIDSelector);
+
+
+    const handleSubmit = () => {
+        console.log("setting up new poll page")
+    }
+
+    //if (questionStatus === 'loading') { return <ComponentLoader /> }
+
     return (
-        <h1>New Question</h1>
+        <Container className="md-6" style={{ marginTop: 20, marginBottom: 20, marginLeft: "auto", marginRight: "auto" }} >
+            <Col>
+                <Row style={{ marginBottom: "48px" }}>
+                    <h2 className="d-flex justify-content-center">Would you rather</h2>
+                    <p className="d-flex justify-content-center">Would you rather</p>
+                </Row>
+                <Row>
+                    {userID && <div>{userID}</div>}
+                    <Button disabled variant="primary" onClick={handleSubmit}>Submit</Button>
+                </Row>
+            </Col>
+        </Container>
     );
 }
 
-export default NewQuestionPage;
+export default NewPollPage;
