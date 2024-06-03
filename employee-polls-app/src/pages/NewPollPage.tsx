0431c5ef69e57b690f552a0d7b3bb2b5deace3c2
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-06-03 16:35:57 +0200

    Implement Form UI

--- employee-polls-app/src/pages/NewPollPage.tsx
+++ employee-polls-app/src/pages/NewPollPage.tsx
@@ -1,9 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { useAppSelector } from "../app/hooks";
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useAppSelector } from "../app/hooks";
 import { userID as userIDSelector } from "../features/authedUser/authedUserSlice";
+
+function PollForm({ label, placeholder }: { label: string, placeholder: string }) {
+    return (
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>{label}</Form.Label>
+            <Form.Control type="text" placeholder={placeholder} />
+        </Form.Group>
+    )
+}
 
 function NewPollPage() {
 
@@ -24,8 +34,11 @@ function NewPollPage() {
                     <p className="d-flex justify-content-center">Would you rather</p>
                 </Row>
                 <Row>
-                    {userID && <div>{userID}</div>}
-                    <Button disabled variant="primary" onClick={handleSubmit}>Submit</Button>
+                    <Form>
+                        <PollForm label="First Option" placeholder="Option One" />
+                        <PollForm label="Second Option"placeholder="Option Two" />
+                        <Button disabled variant="primary" onClick={handleSubmit}>Submit</Button>
+                    </Form>
                 </Row>
             </Col>
         </Container>
