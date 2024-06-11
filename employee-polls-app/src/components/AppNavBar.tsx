b483fec0f63e8762eb262cd2c1863728ded81268
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-06-11 16:31:19 +0200

    Shows logIn/logOut button conditionally

--- employee-polls-app/src/components/AppNavBar.tsx
+++ employee-polls-app/src/components/AppNavBar.tsx
@@ -8,27 +8,40 @@ import ROUTES from '../constants/routes';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import {
     userAvatarURL,
-    userID as authedUserID,
+    isAuthed as isAuthedSelector,
     userName as authedUserName,
 } from "../features/authedUser/authedUserSlice";
 import { logOut } from '../features/authedUser/authedUserSlice';
 import getAvatarImage from '../utils/avatar';
 
+function LoginLogoutButton({
+    isAuthed,
+    logOutHandler,
+    logInHandler,
+}: {
+    isAuthed: () => boolean,
+    logOutHandler: () => void,
+    logInHandler: () => void,
+}) {
+    return (
+        isAuthed() ? <Button variant="outline-success" onClick={logOutHandler}>Logout</Button>
+            : <Button variant="success" onClick={logInHandler}>Login</Button>
+    )
+}
 
 function AppNavBar() {
 
     const avatarURL = useAppSelector(userAvatarURL);
-    const userID = useAppSelector(authedUserID);
     const userName = useAppSelector(authedUserName);
+    const isAuthed = useAppSelector(isAuthedSelector);
     const dispatch = useAppDispatch();
     const handleLogOut = () => dispatch(logOut());
-
-    // todo: add login button to the navbar and apply navigate(ROUTES.LOGIN, { state: { from: location } });
+    const handleLogIn = () => { console.log('Login button clicked') };
 
     return (
         <Navbar expand="lg" className="bg-body-tertiary" style={{ marginBottom: "48px" }}>
             <Container>
-                <Navbar.Brand as={Link} to={ROUTES.HOME}>React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand as={Link} to={ROUTES.HOME}>Employee Polls</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -39,11 +52,11 @@ function AppNavBar() {
                     <div className="d-flex" style={{ marginRight: '24px' }}>
                         <Image style={{ width: '50px', height: '50px', marginRight: '10px' }}
                             src={getAvatarImage(avatarURL)}
-                            alt={`Profile Picture ${userID}`}
+                            alt={`Profile Picture ${userName}`}
                             roundedCircle />
-                        <span className='d-flex align-items-center'>{userName}</span>
+                        {isAuthed && <span className='d-flex align-items-center'>{userName}</span>}
                     </div>
-                    <Button variant="outline-success" onClick={handleLogOut}>Logout</Button>
+                    <LoginLogoutButton isAuthed={() => isAuthed} logOutHandler={handleLogOut} logInHandler={handleLogIn} />
                 </Navbar.Collapse>
             </Container>
         </Navbar>
