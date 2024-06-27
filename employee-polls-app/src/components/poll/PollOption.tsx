028d4d2c970e49d6ee2c6340c22edc52bf205cf9
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-06-27 19:53:59 +0200

    Displays votes percentage on a ProgressBar

--- employee-polls-app/src/components/poll/PollOption.tsx
+++ employee-polls-app/src/components/poll/PollOption.tsx
@@ -2,6 +2,7 @@ import Button from "react-bootstrap/esm/Button";
 import Card from "react-bootstrap/esm/Card";
 import Col from "react-bootstrap/esm/Col";
 import Option from "../../utils/option";
+import ProgressBar from "react-bootstrap/esm/ProgressBar";
 
 const getVotePercentages = (optionVotes: number, totalVotes: number) => totalVotes > 0
     ? (optionVotes / totalVotes) * 100 : 0;
@@ -13,15 +14,29 @@ function PollOption({
     votes,
     clickHandler,
 }: { option: Option, isSelected: boolean, votes: { optionVotes: number, totalVotes: number }, clickHandler: () => void }) {
-    console.log(`votes: ${votes.optionVotes} totalVotes: ${votes.totalVotes} percentage: ${getVotePercentages(votes.optionVotes, votes.totalVotes)}`);
+
+    const votePercentage = getVotePercentages(votes.optionVotes, votes.totalVotes);
 
     return (
         <Col >
             <Card border="success">
                 <Card.Body>
                     <Card.Title className="d-flex justify-content-center">{option.text}</Card.Title>
+                    {(votes.totalVotes > 0) && (
+                        <>
+                            <Card.Text className="d-flex justify-content-center">
+                                {votes.optionVotes} out of {votes.totalVotes} users chose this option
+                            </Card.Text>
+                            <ProgressBar
+                                className="mb-3"
+                                style={{ height: '30px' }}
+                                variant='success'
+                                now={votePercentage}
+                                label={<span style={{fontSize: '1rem' }}>{`${votePercentage.toFixed(2)}%`}</span>}
+                            />
+                        </>
+                    )}
                     <Button className="w-100" variant="success" onClick={clickHandler}>{isSelected && <b>{'\u2713'} </b>}Click</Button>
-                    {(votes.totalVotes > 0) && <Card.Text className="d-flex justify-content-center">{votes.optionVotes} out of {votes.totalVotes} votes</Card.Text>}
                 </Card.Body>
             </Card>
         </Col>
